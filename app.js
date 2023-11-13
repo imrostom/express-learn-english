b88b71c5e256b0db6a5f5d180431ec76905f5720
Rostom <rostomali4444@gmail.com>
2023-11-13 09:48:09 +0600

    make wordlist api pagination

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+var cors = require('cors')
 const dotenv = require('dotenv');
 const db = require('./database/db');
+
 dotenv.config();
 
 const app = express();
-const port = process.env.APP_PORT || 3000;
+app.use(cors())
+const port = process.env.APP_PORT || 5000;
 
 
 app.get('/', (req, res) => {
@@ -18,8 +21,26 @@ app.get('/', (req, res) => {
 app.get('/words', async (req, res) => {
     const perPage = req.query?.per_page || 50;
     const currentPage = req.query?.current_page || 1;
+    const filterBy = req.query?.filter || '';
 
-    const words = await db.select().table('words').paginate({ perPage, currentPage });
+    const words = await db.select()
+        .where('word', 'like', filterBy + '%')
+        .table('words')
+        .paginate({ perPage, currentPage, isLengthAware: true});
+
+    res.json({
+        message: "Success",
+        data: words
+    });
+});
+
+
+app.get('/letter-word-count', async (req, res) => {
+    const words = await db('words')
+        .select(db.raw('SUBSTRING(word, 1, 1) AS letter, COUNT(*) AS count'))
+        .groupBy(db.raw('SUBSTRING(word, 1, 1)'))
+        .orderBy(db.raw('SUBSTRING(word, 1, 1)'));
+
 
     res.json({
         message: "Success",
